54c156cd3115c50fc2f6c709b8147b472f5c0f22
nisheshawale <073bex423.nishesh@pcampus.edu.np>
2020-07-04 11:34:55 +0545

    Model Created

---
+++ sell_in_auction/Auction/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class AuctionItems(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='pictures/', max_length=100)
+    current_bid = models.DecimalField(max_digits=7, decimal_places=2)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    bid_time = models.TimeField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    winner = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
